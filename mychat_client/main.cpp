cf3a794b3664d0ce31909503663cf264efe30839
chen <1136639039@qq.com>
2020-10-27 14:06:30 +0800

    添加注释并整理成笔记

--- mychat_client/main.cpp
+++ mychat_client/main.cpp
@@ -10,7 +10,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #define BUF_SIZE 64
-
+//client
 int main(int argc, char* argv[]) {
 	if (argc <= 2)
 	{
@@ -18,13 +18,13 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	const char* ip = argv[1];
-	int port = atoi(argv[2]);
-	sockaddr_in address;
-	bzero(&address, sizeof(address));
+	int port = atoi(argv[2]);//字符串转整型
+	sockaddr_in address;//tcp/ip专用socket地址
+	bzero(&address, sizeof(address));//bzero与memset效果相同，bzero是linux特有的函数
 	//memset(&address, '\0', sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_port = htons(port);
-	inet_pton(AF_INET, ip, &address.sin_addr);
+	address.sin_family = AF_INET;//设置地址族
+	address.sin_port = htons(port);//htons:host to net short 字节序转换
+	inet_pton(AF_INET, ip, &address.sin_addr);//ip地址转换函数，反过来是inet_ntop
 
 	int ret;
 	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -47,18 +47,19 @@ int main(int argc, char* argv[]) {
 	char msg[BUF_SIZE];
 	while (1)
 	{
-		ret = poll(fds, 2, -1);
+		ret = poll(fds, 2, -1);//返回就绪（可读、可写、异常）的文件描述符的总数，返回-1表示失败并设置了errno
 		if (ret < 0)
 		{
 			printf("poll failure!\n");
 			break;
 		}
-		if (fds[1].revents & POLLRDHUP)
+		//轮询每个文件描述符与事件
+		if (fds[1].revents & POLLRDHUP)//服务器关闭了socket连接
 		{
 			printf("connection closed by server!\n");
 			break;
 		}
-		else if (fds[1].revents & POLLIN)
+		else if (fds[1].revents & POLLIN)//socket端口有输入
 		{
 			memset(msg, '\0', BUF_SIZE);
 			ret = recv(fds[1].fd, msg, BUF_SIZE - 1, 0);
@@ -72,10 +73,11 @@ int main(int argc, char* argv[]) {
 			}
 			printf("%s", msg);
 		}
-		if (fds[0].revents & POLLIN)
+		if (fds[0].revents & POLLIN)//用户直接输入终端
 		{
-			splice(0, NULL, p[1], NULL, 32768, SPLICE_F_MORE);
-			splice(p[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE);
+			//splice()函数可以在两个文件描述符之间移动数据，且其中一个描述符必须是管道描述符
+			splice(0, NULL, p[1], NULL, 32768, SPLICE_F_MORE); //输入零拷贝到管道
+			splice(p[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE);//管道零拷贝到socket
 		}
 	}
 	close(sockfd);
--- mychat_server/main.cpp
+++ mychat_server/main.cpp
@@ -17,6 +17,7 @@ struct client
 	char* write;
 	char buf[BUF_SIZE];
 };
+//server
 int main(int argc, char* argv[]) {
 	if (argc <= 2)
 	{
@@ -38,22 +39,26 @@ int main(int argc, char* argv[]) {
 	ret = bind(listenfd, (sockaddr*)&address, sizeof(address));
 	assert(ret != -1);
 
-	client* USERS = new client[FD_LIMIT];
+	client* USERS = new client[FD_LIMIT];//保存用户数据，可以直接用fd获取对应的用户数据（以空间换时间）
 	int user_count = 0;
-	pollfd fds[USER_LIMIT + 1];
+	pollfd fds[USER_LIMIT + 1];//+1是listen fd
+	//添加并设置listen fd
 	fds[0].fd = listenfd;
 	fds[0].events = POLLIN | POLLERR;
 	fds[0].revents = 0;
+	//添加并设置用户socket
 	for (int i = 1; i < USER_LIMIT + 1; i++)
 	{
 		fds[i].fd = -1;
 		fds[i].events = 0;
 		fds[i].revents = 0;
 	}
+	//开始监听listen fd
 	ret = listen(listenfd, 5);
 	assert(ret != -1);
 	while (1)
 	{
+		//返回就绪（可读、可写、异常）的文件描述符的总数，返回-1表示失败并设置了errno
 		ret = poll(fds, USER_LIMIT + 1, -1);
 		if (ret < 0)
 		{
@@ -66,20 +71,25 @@ int main(int argc, char* argv[]) {
 		}
 		else
 		{
+			//轮询每个文件描述符与事件
 			for (int i = 0; i < user_count + 1; i++)
 			{
+				//监听端口有新的连接
 				if ((fds[i].fd == listenfd) && (fds[i].revents & POLLIN))
 				{
+					//从accept队列中取出一个socket连接
 					int clientfd;
 					sockaddr_in clientAddr;
 					bzero(&clientAddr, sizeof(clientAddr));
 					socklen_t length = (socklen_t)sizeof(clientAddr);
 					clientfd = accept(listenfd, (sockaddr*)&clientAddr, &length);
+					//失败时返回-1并设置errno
 					if (clientfd < 0)
 					{
 						printf("errno:&d\n", errno);
 						continue;
 					}
+					//用户数量超出限制时发送错误信息后关闭连接
 					if (user_count >= USER_LIMIT)
 					{
 						const char* info = "Sorry,too many users\n";
@@ -88,6 +98,7 @@ int main(int argc, char* argv[]) {
 						close(clientfd);
 						continue;
 					}
+					//新增一个用户
 					user_count++;
 					USERS[clientfd].address = clientAddr;
 					fds[user_count].fd = clientfd;
@@ -95,12 +106,14 @@ int main(int argc, char* argv[]) {
 					fds[user_count].revents = 0;
 					printf("comes a new user,now have %d users\n", user_count);
 				}
+				//fds[i]发生错误
 				else if (fds[i].revents & POLLERR)
 				{
 					printf("get an error from %d\n", fds[i].fd);
 					char errors[100];
 					memset(errors, '\0', sizeof(errors));
 					socklen_t length = sizeof(errors);
+					//取出对应socket的错误信息并清除错误，失败返回-1
 					if (getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0)
 					{
 						printf("getsockopt failed,errno:%d\n", errno);
@@ -108,6 +121,7 @@ int main(int argc, char* argv[]) {
 					printf("error:%s\n", errors);
 					continue;
 				}
+				//客户端断开连接
 				else if (fds[i].revents & POLLRDHUP)
 				{
 					USERS[fds[i].fd] = USERS[fds[user_count].fd];
@@ -117,12 +131,14 @@ int main(int argc, char* argv[]) {
 					user_count--;
 					i--;
 				}
+				//客户端发送了信息
 				else if (fds[i].revents & POLLIN)
 				{
 					int clientfd = fds[i].fd;
 					memset(USERS[clientfd].buf, '\0', BUF_SIZE);
+					//接收信息保存到该用户对应的buf
 					ret = recv(clientfd, USERS[clientfd].buf, BUF_SIZE, 0);
-					if (ret < 0)
+					if (ret < 0)//如果不是EAGAIN错误就关闭该客户端连接
 					{
 						if (errno != EAGAIN)
 						{
@@ -138,6 +154,7 @@ int main(int argc, char* argv[]) {
 					{
 						printf("code should not come to here\n");
 					}
+					//设置其它用户的可写事件并设置write指针指向发送消息的用户buf
 					else
 					{
 						printf("Get %d bytes from %d: %s ", ret, fds[i].fd, USERS[fds[i].fd].buf);
@@ -151,6 +168,7 @@ int main(int argc, char* argv[]) {
 						}
 					}
 				}
+				//挨个发送接收到的信息
 				else if (fds[i].revents & POLLOUT)
 				{
 					if (!USERS[fds[i].fd].write)
@@ -170,6 +188,7 @@ int main(int argc, char* argv[]) {
 							i--;
 						}
 					}
+					//回复events
 					USERS[fds[i].fd].write = NULL;
 					fds[i].events |= POLLIN;
 					fds[i].events |= ~POLLOUT;
